d70ccf803887a4d5a4a659a3ee9eb6fc0c94321b
Uki <in2cr7@gmail.com>
2024-01-05 14:55:12 +0530

    Biginning JS

---
+++ script.js
@@ -0,0 +1,54 @@
+var maxValue = Math.max(1,10,100,999);
+console.log('Maximum Value',maxValue);
+var minValue = Math.min(2,34,789);
+console.log('Minimum Value',minValue);
+console.log(Math.round(3.75))
+ console.log(Math.round(3.35))
+ console.log(Math.round(99.1))
+ console.log(Math.floor(3.75))
+console.log(Math.floor(49.5))
+console.log(Math.ceil(3.75))
+ console.log(Math.ceil(99.9))
+ console.log(Math.ceil(3.01))
+console.log(Math.random);
+console.log(Math.random);
+
+var randomNumber = Math.floor(Math.random() *numberOfChoices + firstValue)
+
+
+var numberOfChoices = 12;
+var firstValue =1;
+var randomNumber = Math.floor(Math.random() *numberOfChoices + firstValue);
+console.log('Random Number',randomNumber);
+
+firstValue = 100;
+numberOfChoices = 400;
+randomNumber = Math.floor(Math.random() * numberOfChoices + firstValue);
+console.log('Random Number (100 to 500): ',randomNumber)
+
+console.log(Math.trunc(12.4))
+
+console.log(Math.sign(0))
+console.log(Math.sign(1.7))
+console.log(Math.sign(27))
+console.log(Math.sign(-1.7))
+console.log(Math.sign(-7))
+
+console.log(Math.pow(2,2))
+console.log(Math.pow(2,3))
+console.log(Math.pow(7,2))
+
+console.log(Math.sqrt(4))
+console.log(Math.sqrt(9))
+
+console.log(Math.abs(-7))
+console.log(Math.abs(4))
+console.log(Math.abs(0))
+
+console.log(Math.sin(90 * 180))
+
+console.log(Math.(4))
+// 
+let a = 3;
+let b = new Number(3);
+let c = 3;
